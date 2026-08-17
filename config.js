3c9dba5fec1914130628c6fab20293592d6fdeb0
catzevricxplay <zervicxplay@gmail.com>
2026-08-17 09:17:58 +0530

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-REAL_ENDPOINT: "https://premiumotp.pro/api/v1/stark"  // Original site ka API
-CLONE_ENDPOINT: "/api/v1/stark"  // Tera clone ka API (proxy)
-API_KEY: "ffcc751480b3c67244ea7123acbeb608"  // API ka key
